9dd7871ed5fa20f1bae93b7609ac56975de2617a
Wenfeng Zhu <wenfeng.zhu@campus.lmu.de>
2020-12-30 16:45:41 +0100

    adjust layout and add text inputs

--- WebFunctions.js
+++ WebFunctions.js
@@ -1,19 +1,26 @@
 window.addEventListener('DOMContentLoaded', function () {
     const img = document.getElementById('imageArea');
+    const texts = document.getElementById('inputText')
     const preButton = document.getElementById('preButton');
     const nextButton = document.getElementById('nextButton');
-    const numOfText = 0;
     const memesList = [];
     const areaWidth = img.offsetWidth;
-
-
     const numberOfImages = () => memesList.length;
     let currentImageID = 1;
 
     function showImage(num) {
         let meme = memesList[num];
+        let numOfText = meme.box_count;
         img.innerHTML = '';
+        texts.innerHTML = '';
         img.append(renderImage(meme.url, meme.width, meme.height))
+        for (let i = 1; i <= numOfText; i++) {
+            let input = document.createElement('input');
+            input.id = 'inputText-' + i;
+            input.type = 'text';
+            input.value = 'inputText-' + i;
+            texts.append(input);
+        }
 
     }
 
@@ -22,8 +29,6 @@ window.addEventListener('DOMContentLoaded', function () {
         newImage.src = url;
         newImage.width = areaWidth;
         newImage.height = height * areaWidth / width;
-
-
         return newImage
     }
 
@@ -50,7 +55,6 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     loadImageUrls();
-    console.log(currentImageID);
 
 
 });
