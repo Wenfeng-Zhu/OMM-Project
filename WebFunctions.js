17f88bc46b743990c9601a258f55f7ba80466b45
Wenfeng Zhu <wenfeng.zhu@campus.lmu.de>
2021-01-03 17:59:52 +0100

    adjust layout of text-input area and add the text to the image area

--- WebFunctions.js
+++ WebFunctions.js
@@ -1,36 +1,30 @@
 window.addEventListener('DOMContentLoaded', function () {
-    const img = document.getElementById('imageArea');
+    const imageArea = document.getElementById('imageArea');
     const texts = document.getElementById('inputText')
     const preButton = document.getElementById('preButton');
     const nextButton = document.getElementById('nextButton');
+    const addNewText = document.getElementById('addNewText');
+    const applyButton = document.getElementById('apply');
+    const displayImage = document.createElement('img');
     const memesList = [];
-    const areaWidth = img.offsetWidth;
+    const areaWidth = imageArea.offsetWidth;
     const numberOfImages = () => memesList.length;
+    const inputBoxes = [];
+    let displayTexts = [];
     let currentImageID = 1;
 
     function showImage(num) {
         let meme = memesList[num];
-        let numOfText = meme.box_count;
-        img.innerHTML = '';
-        texts.innerHTML = '';
-        img.append(renderImage(meme.url, meme.width, meme.height))
-        for (let i = 1; i <= numOfText; i++) {
-            let input = document.createElement('input');
-            input.id = 'inputText-' + i;
-            input.type = 'text';
-            input.value = 'inputText-' + i;
-            texts.append(input);
-        }
-
+        displayImage.src = meme.url;
+        displayImage.width = areaWidth;
+        displayImage.height = meme.height * areaWidth / meme.width;
     }
 
-    function renderImage(url, width, height) {
-        const newImage = document.createElement('img');
-        newImage.src = url;
-        newImage.width = areaWidth;
-        newImage.height = height * areaWidth / width;
-        return newImage
-    }
+    // function renderImage(url, width, height) {
+    //     displayImage.src = url;
+    //     displayImage.width = areaWidth;
+    //     displayImage.height = height * areaWidth / width;
+    // }
 
     preButton.addEventListener('click', function () {
         currentImageID = currentImageID == 0 ? numberOfImages() - 1 : currentImageID - 1;
@@ -40,6 +34,23 @@ window.addEventListener('DOMContentLoaded', function () {
         currentImageID = currentImageID == numberOfImages() - 1 ? 0 : currentImageID + 1;
         showImage(currentImageID);
     });
+    addNewText.addEventListener('click', function () {
+        createInputBoxes(inputBoxes.length);
+
+    });
+    applyButton.addEventListener('click', function () {
+        if (inputBoxes.length > displayTexts.length) {
+            let temper = inputBoxes.length - displayTexts.length;
+            for (let i = 0; i < temper; i++) {
+                createDisplayText(displayTexts.length);
+            }
+        }
+
+        updateDisplay();
+
+        //alert('test'+ ' '+ displayTexts[0].innerHTML);
+    })
+
 
     function loadImageUrls() {
         // TODO load meme template images from the Imgflip API
@@ -54,7 +65,47 @@ window.addEventListener('DOMContentLoaded', function () {
             .catch(error => console.log('error', error));
     }
 
+    function textInputTitle() {
+        let input = document.createElement('input');
+        input.height = 50;
+        input.id = 'inputText-title';
+        input.type = 'text';
+        input.value = 'inputText-title';
+        texts.append(input);
+    }
+
+    function createInputBoxes(i) {
+        let input = document.createElement('input');
+        input.height = 50;
+        input.id = 'inputText-' + (i+1);
+        input.type = 'text';
+        input.value = 'inputText-' + (i+1);
+        texts.append(input);
+        inputBoxes[i] = input;
+
+    }
+
+    function createDisplayText(i) {
+        let displayText = document.createElement('text');
+        displayText.id = 'displayText-' + (i+1);
+        displayTexts[i]=displayText;
+        imageArea.append(displayTexts[i]);
+    }
+
+    function updateDisplay() {
+        for (let i = 0; i < displayTexts.length; i++) {
+            displayTexts[i].innerHTML = inputBoxes[i].value;
+        }
+    }
+
+    imageArea.append(displayImage);
     loadImageUrls();
+    textInputTitle();
+    createInputBoxes(0);
+    createInputBoxes(1);
+    createDisplayText(0);
+    createDisplayText(1);
 
 
-});
+})
+;
